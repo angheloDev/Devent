7127a967d8ceca9c47e24902a8e1015fed4af197
angheloDev <120046334+angheloDev@users.noreply.github.com>
2025-10-07 17:39:54 +0800

    added custom font

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,14 @@
-
 const Header = () => {
-  return <div>Header</div>;
+	return (
+		<>
+			<div className='fixed w-full z-10 top-0 h-20 bg-[#0F172A] rounded-b-3xl'>
+				<div className='justify-start text-blue-500 text-3xl font-montserat font-bold'>
+					DEVENT
+				</div>
+			</div>
+			<div className='fixed w-full top-4 h-20 bg-[#0C2869] rounded-b-3xl'></div>
+		</>
+	);
 };
 
 export default Header;
